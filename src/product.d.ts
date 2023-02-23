c59ef0da1294eaa1204951b50874c1011b1aa857
João Carlos Magalhães <joaocarlos.m014@gmail.com>
2023-02-22 22:00:11 -0300

    feat: migrate to vitest

--- src/product.d.ts
+++ src/product.d.ts
@@ -1,18 +1,16 @@
-
 export type TProduct = {
-  product?: string;
-  fases?: string;
-  PCP?: string;
-  explosionOf?: string;
-  cut?: string;
-  checkCut?: string;
-  sublimation?: string;
-  checkSublimation?: string;
-  sew?: string;
-  checkSublimation?: string;
-  checkSew?: string;
-  applyOf?: string;
-  passadoria?: string;
-  packUp?: string;
-  dpa?: string;
-};
+  product?: string
+  fases?: string
+  PCP?: string
+  explosionOf?: string
+  cut?: string
+  checkCut?: string
+  sublimation?: string
+  sew?: string
+  checkSublimation?: string
+  checkSew?: string
+  applyOf?: string
+  passadoria?: string
+  packUp?: string
+  dpa?: string
+}
--- src/styles.ts
+++ src/styles.ts
@@ -1,13 +1,10 @@
-
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-import styled from "styled-components";
+import styled from "styled-components"
 
 export const Cointainer = styled.div`
   display: flex;
   flex-direction: column;
-    height: fit-content;
-      align-items: center;
+  height: fit-content;
+  align-items: center;
   @media ${({ theme }) => theme.devices.tablet} {
   }
-`;
- 
+`
